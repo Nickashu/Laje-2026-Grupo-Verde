cee5ff80c80e3ef2d26df4644f547b001a6428b5
Eduardo Tavares <etavaresg@gmail.com>
2026-07-27 23:04:20 -0300

    movimentação, cena teste, configs de projeto
    implementei a movimentação do player e fiz alterações nas configurações do projeto pra deixar a pixel art sem serrilhamento

--- character_body_2d.gd
+++ character_body_2d.gd
@@ -1,25 +1,31 @@
 extends CharacterBody2D
 
+var velocidade = Vector2(0,0) #velocidade normal no plano cartesiano é zero
+@export var frozen = false
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const VELOCIDADE_MAX = 2
+const ACELERACAO = 15
+const ATRITO = 10
+# aceleracao e atrito pra adicionar fluidez ao movimento
 
+func _physics_process(delta):
+	if frozen:
+		return
+	
+	var resultante = Vector2(0,0)	
+	resultante.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	resultante.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	# operação de subtração pra decidir a direção da resultante x e y (0, 1 ou -1)
+	resultante = resultante.normalized()
+	# faz com que a direcao, quando sobreposta (cima ou baixo com esquerda ou direita), nao fique mais rapido
+	
+	if resultante != Vector2(0,0):
+		
+		velocidade = velocidade.move_toward(resultante * VELOCIDADE_MAX, ACELERACAO * delta)
+		# faz com que a velocidade va gradualmente em direcao a velocidade maxima baseado na aceleracao 
+		# (quanto maior a aceleracao, mais rapido a velocidade chega na quantidade máxima)
+		
+	else: 
+		velocidade = velocidade.move_toward(Vector2.ZERO, ATRITO * delta)
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
-	move_and_slide()
+	move_and_collide(velocidade)
