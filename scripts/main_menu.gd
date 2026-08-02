d38b80821f77040df27e98f42637af25838b5681
Cencari <cencari@proton.me>
2026-08-02 17:34:09 -0300

    transicação correta do main_menu

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,10 +1,11 @@
 extends Control
 
 @onready var select = $AudioStreamPlayer2D
+@onready var transicao = $"Transição"
 
 func _on_play_pressed() -> void:
 	select.play()
-	Transição.load_scene("res://cenas/cena_teste.tscn")
+	transicao.load_scene("res://cenas/cena_teste.tscn")
 
 func _on_quit_pressed() -> void:
 	get_tree().quit()
